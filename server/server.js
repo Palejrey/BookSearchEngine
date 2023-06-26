1239d21994a604b21feeab18c43b44512e52f1a4
palejrey@gmail.com <palejrey@gmail.com>
2023-06-25 20:23:13 -0500

    To implement Apollo Server and apply it as middleware to the Express server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schema');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +15,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Apply the Apollo Server as middleware to Express
+server.applyMiddleware({ app });
 
 app.use(routes);
 
